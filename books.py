a1f317ec4888baa1a53bab6ec5a908b9c18995f6
Kenneth Gallo <kenneth.gallo@mscdirect.com>
2025-08-20 11:14:27 -0400

    adding func to get single book by title

--- books.py
+++ books.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-# Basic project to learn fastapi
+
 app = FastAPI()
 
 BOOKS = [
@@ -11,6 +11,12 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'},
 ]
 
-@app.get("/books/{dynamic_param}")
+@app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.get("/books/{book_title}")
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
