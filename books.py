1379bed9d85009d515cbbde94c5e055cf7cc7750
Kenneth Gallo <kenneth.gallo@mscdirect.com>
2025-08-21 11:48:21 -0400

    Final commit, added basic logic to delete book and basic logic to retrieve by author

--- books.py
+++ books.py
@@ -38,6 +38,14 @@ async def read_author_category_by_query(book_author: str, category: str):
             books_to_return.append(book)
     return books_to_return
 
+@app.get("/books/byauthor/{book_author}")
+async def read_author(book_author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
 @app.post("/books/create_book")
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
@@ -48,3 +56,10 @@ async def update_book(updated_book=Body()):
         if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
             BOOKS[i] = updated_book
 
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
