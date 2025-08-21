c5f974e7cf20ffbf42aac083104e84cc69e52d6a
Kenneth Gallo <kenneth.gallo@mscdirect.com>
2025-08-21 19:57:53 -0400

    setting up enhanced features such as validation

---
+++ books2.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Body
+
+app = FastAPI()
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'kennyg', 'A very nice book!', 5),
+    Book(2, 'Be Fast With FastAPI', 'kennyg', 'A great book!', 5),
+    Book(3, 'Master Endpoints', 'kennyg', 'A awesome book!', 5),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
+    Book(5, 'HP2', 'Author 2', 'A very nice book!', 3),
+    Book(6, 'HP3', 'Author 3', 'A very nice book!', 1),
+
+]
+
+@app.get("/books")
+async def read_all_books():
+    return BOOKS
+
+@app.post("/create-book")
+async def create_book(book_request=Body()):
+    BOOKS.append(book_request)
